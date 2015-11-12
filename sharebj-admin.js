7fdec24c4f22ee11cc33b3c44852fe9af7a4202c
颜俊标 <byrne.yan@yahoo.com>
2015-11-12 11:50:53 +0800

    initial

---
+++ sharebj-admin.js
@@ -0,0 +1,15 @@
+if (Meteor.isClient) {
+
+  Template.login.helpers({
+  });
+
+  Template.login.events({
+
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
